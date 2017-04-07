01cef8d0f9898191dd12a4ef7505f7fc4659a7f8
Yeremia <yeremia@Yeremias-MacBook-Pro.local>
2017-04-07 00:00:38 -0400

    Added base implementations for integrator.cpp

--- xcode/Integrator.cpp
+++ xcode/Integrator.cpp
@@ -7,3 +7,30 @@
 //
 
 #include "Integrator.h"
+
+vec2 Integrator::LeapFrog(vec2 acc, vec2 vel, vec2 pos) {
+    vec2 nextPos = vec2();
+    
+    return nextPos;
+}
+
+// 2nd Order Runge-Kutta
+vec2 Integrator::RungeKutta2(vec2 acc, vec2 vel, vec2 pos) {
+    vec2 nextPos = vec2();
+    
+    return nextPos;
+}
+
+// 4th Order Runge-Kutta
+vec2 Integrator::RungeKutta4(vec2 acc, vec2 vel, vec2 pos) {
+    vec2 nextPos = vec2();
+    
+    return nextPos;
+}
+
+// Explicit Euler Method
+vec2 Integrator::ExpEuler(vec2 acc, vec2 vel, vec2 pos) {
+    vec2 nextPos = vec2();
+    
+    return nextPos;
+}
