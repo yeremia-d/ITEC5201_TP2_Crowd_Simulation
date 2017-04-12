7298d9da128b7e66d5023ffcccfa920273c40818
Yeremia <yeremia@dhcp-125-247.cu-wireless-staff.carleton.ca>
2017-04-12 17:38:36 -0400

    Made integrator methods static

--- xcode/Integrator.cpp
+++ xcode/Integrator.cpp
@@ -7,35 +7,36 @@
 //
 
 #include "Integrator.h"
+namespace Integrator {
+    vec2 Integrator::LeapFrog(vec2 acc, vec2 vel, vec2 pos) {
+        vec2 nextPos = vec2();
+        
+        return nextPos;
+    }
 
-vec2 Integrator::LeapFrog(vec2 acc, vec2 vel, vec2 pos) {
-    vec2 nextPos = vec2();
-    
-    return nextPos;
-}
+    // 2nd Order Runge-Kutta
+    vec2 Integrator::RungeKutta2(vec2 acc, vec2 vel, vec2 pos) {
+        vec2 nextPos = vec2();
+        
+        return nextPos;
+    }
 
-// 2nd Order Runge-Kutta
-vec2 Integrator::RungeKutta2(vec2 acc, vec2 vel, vec2 pos) {
-    vec2 nextPos = vec2();
-    
-    return nextPos;
-}
+    // 4th Order Runge-Kutta
+    vec2 Integrator::RungeKutta4(vec2 dVal, vec2 initVal, int subStep) {
+        vec2 output = vec2();
+        vec2 k1, k2, k3, k4;
+        
+        return output;
+    }
 
-// 4th Order Runge-Kutta
-vec2 Integrator::RungeKutta4(vec2 dVal, vec2 initVal, int subStep) {
-    vec2 output = vec2();
-    vec2 k1, k2, k3, k4;
-    
-    return output;
-}
-
-// Explicit Euler Method
-vec2 Integrator::ExpEuler(vec2 dVal, vec2 initVal, int subStep) {
-    
-    vec2 output = vec2();
-    
-    output.x = initVal.x + dVal.x/subStep;
-    output.y = initVal.y + dVal.y/subStep;
-    
-    return output;
+    // Explicit Euler Method
+    vec2 Integrator::ExpEuler(vec2 dVal, vec2 initVal, int subStep) {
+        
+        vec2 output = vec2();
+        
+        output.x = initVal.x + dVal.x/subStep;
+        output.y = initVal.y + dVal.y/subStep;
+        
+        return output;
+    }
 }
--- xcode/Integrator.h
+++ xcode/Integrator.h
@@ -14,24 +14,24 @@
 using namespace ci;
 
 // Integration Service that takes in an initial position, vel, acc and returns a new position
-
-class Integrator {
-    
-public:
-    
-    // Leapfrog Integration Method
-    vec2 LeapFrog(vec2 acc, vec2 vel, vec2 pos);
-    
-    // 2nd Order Runge-Kutta
-    vec2 RungeKutta2(vec2 acc, vec2 vel, vec2 pos);
-    
-    // 4th Order Runge-Kutta
-    vec2 RungeKutta4(vec2 dVal, vec2 initVal, int subStep);
-    
-    // Explicit Euler Method
-    vec2 ExpEuler(vec2 dVal, vec2 initVal, int subStep);
-    
-};
-
+namespace Integrator {
+    class Integrator {
+        
+    public:
+        
+        // Leapfrog Integration Method
+        static vec2 LeapFrog(vec2 acc, vec2 vel, vec2 pos);
+        
+        // 2nd Order Runge-Kutta
+        static vec2 RungeKutta2(vec2 acc, vec2 vel, vec2 pos);
+        
+        // 4th Order Runge-Kutta
+        static vec2 RungeKutta4(vec2 dVal, vec2 initVal, int subStep);
+        
+        // Explicit Euler Method
+        static vec2 ExpEuler(vec2 dVal, vec2 initVal, int subStep);
+        
+    };
+}
 #endif /* Integrator_hpp */
 
